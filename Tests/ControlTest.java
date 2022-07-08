874c2c87e1195229e82fe0ed8623d7f0254c8dce
Gurjot <gurjotk@localhost>
2022-07-08 16:41:11 -0700

    Tests created

--- Tests/ControlTest.java
+++ Tests/ControlTest.java
@@ -1,5 +1,31 @@
 package Tests;
 
 public class ControlTest {
+	
+	@Test
+	static String testSetNext(String next){
+        fail("Not yet implemented");
+    }
+
+	@Test
+    static String testClear(){
+    	fail("Not yet implemented");
+    }
+
+	@Test
+    static String testBackspace(){
+    	fail("Not yet implemented");
+    	}
+
+	@Test
+    static void testSaveResult(String ans){
+    	fail("Not yet implemented");
+    }
+
+	@Test
+    static String testGetResult(){
+    	fail("Not yet implemented");
+    }
     
 }
+
