a564933eaeaffa0c0bd0f56e73594ddbe41047ad
Gurjot <gurjotk@10.0.0.232>
2022-07-08 17:14:43 -0700

    updated tests

--- Tests/ControlTest.java
+++ Tests/ControlTest.java
@@ -1,31 +1,46 @@
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
 package Tests;
 
 public class ControlTest {
 	
 	@Test
 	static String testSetNext(String next){
+		assertEquals(expression, setNext(true)));
         fail("Not yet implemented");
+        
     }
 
 	@Test
     static String testClear(){
+		assertEquals(expression, clear(true)));
     	fail("Not yet implemented");
     }
 
 	@Test
     static String testBackspace(){
+		assertEquals(expression, backspace(true)));
     	fail("Not yet implemented");
     	}
 
 	@Test
     static void testSaveResult(String ans){
+		assertEquals(expression, saveResult(true)));
     	fail("Not yet implemented");
     }
 
 	@Test
     static String testGetResult(){
+		assertEquals(expression, getResult(true)));
     	fail("Not yet implemented");
     }
     
+	@Test
+    static String testSolveExp(){
+		assertEquals(ans, solveExp(true)));
+    	fail("Not yet implemented");
+    }
 }
 
